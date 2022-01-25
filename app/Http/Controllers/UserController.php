ce073c97ff08f11f5d1640ec26143d509293da41
Hovakimyannn <harutyunhovakimyan7@gmail.com>
2022-01-25 12:44:45 +0400

    Add ajax

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,55 +8,92 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+    public function welcome()
+    {
+        if (!Auth::check()) {
+            return view('welcome');
+        } else {
+            return redirect()->back();
+        }
+    }
+
     public function create()
     {
-        return view('user.create');
+        if (Auth::check()) {
+            return redirect()->route('home');
+        } else {
+            return view('user.create');
+        }
     }
 
     public function store(Request $request)
     {
         $request->validate([
-          'name'=>'required|min:2|max:20',
-          'email'=>'required|email|unique:users',
-          'password'=>'required|confirmed',
+            'name' => 'required|min:2|max:20',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed',
         ]);
 
         $user = User::create([
-           'name'=>$request->name,
-           'email'=>$request->email,
-           'password'=>bcrypt($request->password),
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
         ]);
-        session()->flash('success','Registration passed');
+        session()->flash('success', 'Registration passed');
         Auth::login($user);
-        return view('user.mainPage');
+        return view('user.userPage');
     }
 
     public function loginForm()
     {
-        return view('user.login');
+        if (Auth::check()) {
+            return redirect()->route('home');
+        } else {
+            return view('user.login');
+        }
     }
 
     public function login(Request $request)
     {
         $request->validate([
-           'email'=>'required|email',
-           'password'=>'required',
+            'email' => 'required|email',
+            'password' => 'required',
         ]);
 
         if (Auth::attempt([
-            'email'=>$request->email,
-            'password'=>$request->password,
-        ]))
-        {
-            session()->flash('success','You are logged');
-            return redirect()->home();
+            'email' => $request->email,
+            'password' => $request->password,
+        ])) {
+            session()->flash('success', 'You are logged');
+            return redirect()->route('home');
         }
-        return redirect()->back()->with('error','Incorrect login or password');
+        return redirect()->back()->with('error', 'Incorrect login or password');
     }
 
     public function logout()
     {
         Auth::logout();
         return redirect()->route('welcome');
+    }
+
+    public function changeUserImage(Request $request)
+    {
+        $request->validate([
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+        $path = $request->file('image');
+        $originalName = $path->getClientOriginalName();
+        $employee = Auth::user();
+
+        $employee->update([
+            'userImage' => $originalName,
+        ]);
+        $path->storeAs('public/', "storage/$originalName");
+
+        return  response()->json([
+            'success' => "Image Upload Successfully",
+            'uploaded_image' => "<img src='storage/$originalName' class='userImage' />",
+        ]);
+        //return redirect()->route('home');
     }
 }
--- resources/views/mainPage.blade.php
+++ resources/views/mainPage.blade.php
@@ -20,45 +20,9 @@
             border-radius: 5px;
             padding: 5px 10px;
         }
-
-        table {
-            border-collapse: collapse;
-            width: 60%;
-            margin: 100px auto;
-        }
-
-        th, td {
-            text-align: left;
-            padding: 8px;
-        }
-
-        tr:nth-child(even) {
-            background-color: #D6EEEE;
-        }
-
-        .hidden {
-            display: none;
-        }
-
-        .delete-button:hover {
-            color: whitesmoke;
-            background-color: red;
-        }
-
-        .delete-button{
-            border-radius: 5px;
-        }
     </style>
 </head>
 <body>
-<table>
-    <tr>
-        <th>ID</th>
-        <th>User_Name</th>
-        <th>user Email</th>
-        <th>Password</th>
-    </tr>
-</table>
 <a href="{{route('logout')}}">Log Out</a>
 </body>
 </html>
