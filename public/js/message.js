6ad416064c7de3c814cfe39858e60274357e06f2
Hovakimyannn <harutyunhovakimyan7@gmail.com>
2022-01-28 21:56:37 +0400

    Add ajax3

---
+++ public/js/message.js
@@ -0,0 +1,28 @@
+function message() {
+    let XMLHttp = new XMLHttpRequest();
+    let place = document.getElementById('place-for-message');
+    XMLHttp.onreadystatechange = function () {
+        if (XMLHttp.status === 200 && XMLHttp.readyState === 4) {
+            if (XMLHttp.responseText) {
+                place.innerHTML =
+                    `<div class='message-border' id="close">
+                        <div class='top-border'>
+                            <button class='close-button fa fa-close' onclick="hiddenMessage()">
+                            </button>
+                        </div>
+                        <div class='message-content'>
+                            <p id='message' class='message-text'>` + XMLHttp.responseText + `</p>
+                        </div>
+                    </div>`;
+            }
+        }
+    }
+    XMLHttp.open("POST", '/message', true);
+    XMLHttp.setRequestHeader('Accept', 'text/plain')
+    XMLHttp.send();
+}
+
+function hiddenMessage() {
+    let messageElement = document.getElementById('close');
+    messageElement.remove();
+}
